677d7e5ff8f40f0a3a350c363a6c7f00b6f2867d
Marianna Babayan <marianna_babayan@edu.aua.am>
2017-09-10 02:32:48 -0800

    Initial commit

---
+++ homework2.js
@@ -0,0 +1,47 @@
+alert("Welcome to Math");
+//Variables
+{
+const mn='math';
+console.log(mn);
+}
+
+{
+	const ex='this is YVN';
+	console.log(ex);
+}
+
+{
+	const num='5';
+	console.log(num*5);
+}
+//A number printed in console
+{
+	console.log(135);
+}
+
+//Arithmetic Operations
+{
+	console.log(((5+8)/1)*2);
+}
+
+{
+	console.log(215*5+6);
+}
+
+{
+	const num2='21';
+	console.log(num2/7);
+}
+
+//NAN
+{
+	console.log("M"*135);
+}
+
+//True or False
+{
+	console.log(125===125);
+}
+{
+	console.log(125!==125);
+}
